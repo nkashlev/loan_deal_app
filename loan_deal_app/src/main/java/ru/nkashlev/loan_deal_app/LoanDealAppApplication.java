791d6ee4b9d9e5d5fe2ad092fd29c2a9cf1a80d5
nkashlev <kashlev-1996@mail.ru>
2023-08-09 21:40:25 +0300

    Initial commit

---
+++ loan_deal_app/src/main/java/ru/nkashlev/loan_deal_app/LoanDealAppApplication.java
@@ -0,0 +1,13 @@
+package ru.nkashlev.loan_deal_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanDealAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanDealAppApplication.class, args);
+	}
+
+}
---
+++ loan_deal_app/src/test/java/ru/nkashlev/loan_deal_app/LoanDealAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nkashlev.loan_deal_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanDealAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
